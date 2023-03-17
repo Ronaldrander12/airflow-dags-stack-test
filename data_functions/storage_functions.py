3c2594a3d79a26f05d32f7bf56abe5b0b3872679
Ronald Rander <86743672+Ronaldrander12@users.noreply.github.com>
2023-03-17 15:56:43 -0300

    Create storage_functions.py

---
+++ data_functions/storage_functions.py
@@ -0,0 +1,18 @@
+import os
+import logging
+From pathlib import Path
+
+import pandas as pd
+from pandas import DataFrame
+from google.cloud import bigquery
+from google.cloud import storage
+
+from data_functions import credentials
+
+class Bigquery():
+  def __init__():
+    pass
+  
+class Storage():
+  def __init()__:
+    pass
