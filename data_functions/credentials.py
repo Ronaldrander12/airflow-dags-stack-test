3be5a037971bf88936d010c41848d909c2c94607
Ronald Rander <86743672+Ronaldrander12@users.noreply.github.com>
2023-03-17 15:53:03 -0300

    Create credentials.py

---
+++ data_functions/credentials.py
@@ -0,0 +1,11 @@
+import os
+import yaml
+from google.cloud import secretmanager
+import json
+from google.oauth2 import service_account
+from pathlib import Path
+
+class Credentials():
+  def __init__():
+    pass
+  
